c61297c66e56f146ba81739c01d4693ffecafe30
Kasaklalita <kasaklalita@mail.ru>
2022-12-18 15:57:18 +0300

    decorators with parameters

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -4,13 +4,18 @@
 #   return inner_function
 
 def decorator_function(original_function):
-  def wrapper_function():
+  def wrapper_function(*args, **kwargs):
     print('wrapper executed this before {}'.format(original_function.__name__))
-    original_function()
+    original_function(*args, **kwargs)
   return wrapper_function
 
 @decorator_function
 def display():
   print('display function ran')
 
+@decorator_function
+def display_info(name, age):
+  print('display_info ran with arguments ({} {})'.format(name, age))
+
 display()
+display_info('John', 25)
