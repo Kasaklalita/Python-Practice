805533d341429303ed7debe86624301570d13fc8
Kasaklalita <kasaklalita@mail.ru>
2022-12-18 16:01:30 +0300

    class-based decorators

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -9,11 +9,19 @@ def decorator_function(original_function):
     original_function(*args, **kwargs)
   return wrapper_function
 
-@decorator_function
+class decorator_class(object):
+  def __init__(self, original_function):
+    self.original_function = original_function
+  
+  def __call__(self, *args, **kwargs):
+    print('call method executed this before {}'.format(self.original_function.__name__))
+    self.original_function(*args, **kwargs)
+
+@decorator_class
 def display():
   print('display function ran')
 
-@decorator_function
+@decorator_class
 def display_info(name, age):
   print('display_info ran with arguments ({} {})'.format(name, age))
 
